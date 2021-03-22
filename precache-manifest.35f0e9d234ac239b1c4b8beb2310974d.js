79232af431757fae8ceb70c780d80c254e158efb
Anna <garsinaanna32778@gmail.com>
2021-03-22 20:48:10 +0200

    Updates

---
+++ precache-manifest.35f0e9d234ac239b1c4b8beb2310974d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "486b02e7306717c8faebaace11d3598e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7c7a19eaf806aa4e071d",
+    "url": "./static/js/2.cad20d42.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.cad20d42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11d6eb2b3383f9f222f4",
+    "url": "./static/js/main.a95c5a71.chunk.js"
+  },
+  {
+    "revision": "2a286f7172708a7e8c5a",
+    "url": "./static/js/runtime-main.3a2c9fb2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.35f0e9d234ac239b1c4b8beb2310974d.js"
 );
 
 self.addEventListener('message', (event) => {
